4488feb60dc0f55e6f8a3a748265be8011f66afc
Kevin Redmond <kred1150@gmail.com>
2023-07-19 12:21:22 -0500

    Added a search bar to filter the list of Pokemon based on a given text entry.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,28 @@
-<script>
-export default {
-  data: () => ({
-    pokemonList: [],
-  }),
-  async mounted() {
-    const pokeData = await fetch("https://pokeapi.co/api/v2/pokedex/2/").then((response) => response.json());
-    this.pokemonList = pokeData.pokemon_entries;
-  },
-};
+<script setup>
+import { ref, computed, onMounted } from "vue";
+
+const pokemonList = ref([]);
+const searchPokemon = ref("");
+
+const filteredPokemonList = computed(() => {
+  return pokemonList.value.filter((pokemon) => {
+    return pokemon.pokemon_species.name.includes(searchPokemon.value);
+  });
+});
+
+onMounted(async () => {
+  const pokeData = await fetch("https://pokeapi.co/api/v2/pokedex/2/").then((response) => response.json());
+  pokemonList.value = pokeData.pokemon_entries;
+});
 </script>
 
 <template>
   <h1>Pokedex</h1>
 
-  <li v-for="pokemon in pokemonList" key:pokemon>
+  <p>{{ searchPokemon }}</p>
+  <input type="text" v-model="searchPokemon" />
+
+  <li v-for="pokemon in filteredPokemonList" key:pokemon>
     ID: {{ pokemon.entry_number }} Name: {{ pokemon.pokemon_species.name }}
   </li>
 </template>
